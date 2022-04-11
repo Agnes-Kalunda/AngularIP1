9f1175f2fc4aa64ade82c18a38018af41735ce2b
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-04-11 04:00:12 +0300

    edited app.component specs

--- src/app/quote-box/quote-box.component.ts
+++ src/app/quote-box/quote-box.component.ts
@@ -1,12 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-quote-box',
-  templateUrl: './quote-box.component.html',
-  styleUrls: ['./quote-box.component.css']
+  selector: "app-quote-box",
+  templateUrl: "./quote-box.component.html",
+  styleUrls: ["./quote-box.component.scss"],
 })
-export class QuoteBoxComponent implements {
-
+export class QuoteBoxComponent {
   @Input() author!: string;
   @Input() quote!: string;
   @Input() tweetURL!: string;
